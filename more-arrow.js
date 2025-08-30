04ce40cac98df71ebe4ed9d451ed268710cdfe3b
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-30 09:37:27 +0600

    more-arrow

---
+++ more-arrow.js
@@ -0,0 +1,24 @@
+const getMax = (amount, taxRate) => (amount * taxRate) / 100;
+const add = (p, q) => p + q;
+
+const tax = getMax(100, 2);
+
+// single parameter
+
+const getSquare = (x) => x * x;
+const getHalf = (num) => num / 2;
+const getArray = (nums) => nums[0];
+const logIt = () => console.log(78);
+
+console.log(getHalf(6));
+console.log(getSquare(8));
+console.log(getArray([9, 19, 52, 20, 28]));
+console.log(logIt());
+
+// Annonymous Function
+
+// document.getElementById('').addEventListener('click', () => {
+
+// })
+
+document.getElementById("btn-click").addEventListener("click", (event) => {});
